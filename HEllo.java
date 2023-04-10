e32b0232a55f58faf1fac752d4b6f592b43be10c
githh <1944142700@qq.com>
2023-04-10 15:28:33 +0800

    HEllo.java

---
+++ HEllo.java
@@ -0,0 +1,7 @@
+public static void main(String []args){
+	System.out.println("hekki world line1");
+	System.out.println("hekki world line1");
+	System.out.println("hekki world line1");
+	System.out.println("hekki world line1");
+}
+
