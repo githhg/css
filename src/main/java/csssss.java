90b5112a9af03deb48054bf382325a374385cd0f
sees <1944142700@qq.com>
2023-04-11 15:14:48 +0800

    新增

---
+++ src/main/java/csssss.java
@@ -0,0 +1,5 @@
+public class csssss {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
--- src/main/java/gggg.java
+++ src/main/java/gggg.java
@@ -1,2 +1,5 @@
 public class gggg {
+    public static void main(String[] args) {
+        System.out.println("123123");
+    }
 }
